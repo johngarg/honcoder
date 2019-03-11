c2ba386244302807d0ec0ce8c8e3376657dad4b5
John Gargalionis <johngargalionis@gmail.com>
2019-03-11 15:35:11 +1100

    Using xgoogle now

--- honcoder.py
+++ honcoder.py
@@ -1,15 +1,16 @@
 import hashlib
 import pickle
 import pprint
-from googlesearch import search
+import googlesearch
 import argparse
+from xgoogle.search import GoogleSearch
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--search', default=None, help='Search term')
-parser.add_argument('--n', default=None, help='Number of urls to pull from google search')
-parser.add_argument('--lang', default='en', help='Language for google search')
+parser.add_argument('--n', default=None, help='Number of urls to pull from search')
+parser.add_argument('--lang', default='en', help='Language for search')
+parser.add_argument('--engine', default='google', choices=['google', 'duckduckgo'])
 args = parser.parse_args()
-
 
 def process_url(url):
     """Function to process string into format preferred by honcode."""
@@ -37,24 +38,46 @@ def is_HONcode_certified(url, path='data/'):
     else:
         return False
 
+def honcode_map(result):
+    """Return 1 if HONcode certified, 2 if not."""
+    return 1 if result else 2
 
-if args.search is None:
-    raise ValueError('You need to input a search term!')
-else:
-    search_term = args.search 
+def display_results(url, counter):
+    """Prints results of search"""
+    print(f'Url number: {counter+1} of {n_urls}')
+    print(f'Whole url: {url}')
+    print(f'Processed url: {process_url(url)}')
+    print(f'Is certified? {is_HONcode_certified(url)}')
+    print('--------------------------------------------------------------------------------')
 
-if args.n is None:
-    print('Number of urls not specified, defaulting to 50...')
-    n_urls = 50
-else:
-    n_urls = int(args.n)
+def results_to_csv(args, url, counter):
+    if counter == 0:
+        print(f'Count, Search engine, Search term, Whole url, Processed url, Language, Tertile, Is certified?, Website')
+
+    tertile = (counter // 50) + 1
+    processed_url = process_url(url)
+
+    print(f'{counter}, {args.engine}, {args.search}, {url}, {processed_url}, {args.lang}, {tertile}, {honcode_map(is_HONcode_certified(url))}, ')
 
 if __name__ == '__main__':
+
+    if args.search is None:
+        raise ValueError('You need to input a search term!')
+    else:
+        search_term = args.search
+
+    if args.n is None:
+        print('Number of urls not specified, running on 50...')
+        n_urls = 50
+    else:
+        n_urls = int(args.n)
+
+    gs = GoogleSearch(search_term, lang=args.lang)
+    gs.results_per_page = 10
     counter = 0
-    for url in search(search_term, stop=n_urls, lang=args.lang):
-        print(f'Url number: {counter+1} of {n_urls}')
-        print(f'Whole url: {url}')
-        print(f'Processed url: {process_url(url)}')
-        print(f'Is certified? {is_HONcode_certified(url)}')
-        print('--------------------------------------------------------------------------------')
-        counter += 1
+    while counter < int(args.n):
+        results = gs.get_results()
+        for result in results:
+            url = result.url
+            results_to_csv(args, url, counter)
+            counter += 1
