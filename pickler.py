4b64755da5d4706febd024d72997d6ab81a36610
John Gargalionis <johngargalionis@gmail.com>
2019-07-23 15:28:30 +1000

    Fix formatting

--- pickler.py
+++ pickler.py
@@ -1,31 +1,37 @@
 """Script to download honcode database as pickled python dictionary"""
 
 
+import argparse
 import os
 import pickle
-import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--path', default=None, help='Desired database location.')
+parser.add_argument("--path", default=None, help="Desired database location.")
 args = parser.parse_args()
 
 
-def download_honcode_database(path='data/'):
+def download_honcode_database(path="data/"):
     """downloads honcode database as .txt file to `path`"""
-    os.system(f'cd {path}; wget https://www.honcode.ch/HONcode/Plugin/listeMD5.txt')
+    os.system(f"cd {path}; wget https://www.honcode.ch/HONcode/Plugin/listeMD5.txt")
 
-def pickle_honcode_database(path='data/'):
+
+def pickle_honcode_database(path="data/"):
     """pickles honcode .txt database at `path`"""
     d = {}
-    with open(f'{path}listeMD5.txt', 'r', encoding='utf-8') as md5_file:
+    with open(f"{path}listeMD5.txt", "r", encoding="utf-8") as md5_file:
         for line in md5_file:
             if len(line.split()) == 2:
                 url_hash, hon_code = line.split()
                 d[url_hash] = hon_code
-    with open(f'{path}honcode.dat', 'wb+') as dat_file:
+    with open(f"{path}honcode.dat", "wb+") as dat_file:
         pickle.dump(d, dat_file)
-        print(f'Data written to {path}honcode.dat')
+        print(f"Data written to {path}honcode.dat")
 
-if __name__ == '__main__':
-    download_honcode_database(args.path) if args.path is not None else download_honcode_database()
-    pickle_honcode_database(args.path) if args.path is not None else pickle_honcode_database()
+
+if __name__ == "__main__":
+    download_honcode_database(
+        args.path
+    ) if args.path is not None else download_honcode_database()
+    pickle_honcode_database(
+        args.path
+    ) if args.path is not None else pickle_honcode_database()
